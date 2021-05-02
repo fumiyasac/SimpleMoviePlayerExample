54cc39b3b5e0f584b473744fe4a12c49eb39b9e1
Fumiya Sakai <just1factory@gmail.com>
2021-05-03 05:19:43 +0900

    initial commit

---
+++ SimpleMoviePlayerExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleMoviePlayerExample
+//
+//  Created by 酒井文也 on 2021/05/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
