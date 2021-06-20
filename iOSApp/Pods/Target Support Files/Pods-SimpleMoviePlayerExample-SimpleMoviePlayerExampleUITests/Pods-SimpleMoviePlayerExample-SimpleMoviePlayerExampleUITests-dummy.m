647b363a3915a0364fa44ef81af1ad32919124e0
Fumiya Sakai <just1factory@gmail.com>
2021-06-21 04:58:58 +0900

    Podsによるライブラリの新規追加

---
+++ iOSApp/Pods/Target Support Files/Pods-SimpleMoviePlayerExample-SimpleMoviePlayerExampleUITests/Pods-SimpleMoviePlayerExample-SimpleMoviePlayerExampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SimpleMoviePlayerExample_SimpleMoviePlayerExampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SimpleMoviePlayerExample_SimpleMoviePlayerExampleUITests
+@end
---
+++ iOSApp/SimpleMoviePlayerExampleTests/Mock.generated.swift
@@ -0,0 +1,15 @@
+// Generated using Sourcery 1.0.2 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+
+
+// Generated with SwiftyMocky 4.0.1
+
+import SwiftyMocky
+import XCTest
+import Foundation
+@testable import SimpleMoviePlayerExample
+
+
+// SwiftyMocky: no AutoMockable found.
+// Please define and inherit from AutoMockable, or annotate protocols to be mocked
