c9ed5d1405077b28d94754671e55a6ca79f56324
Fumiya Sakai <just1factory@gmail.com>
2021-06-21 05:00:41 +0900

    delete caches

--- iOSApp/Pods/Target Support Files/Pods-SimpleMoviePlayerExample-SimpleMoviePlayerExampleUITests/Pods-SimpleMoviePlayerExample-SimpleMoviePlayerExampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SimpleMoviePlayerExample_SimpleMoviePlayerExampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SimpleMoviePlayerExample_SimpleMoviePlayerExampleUITests
-@end
