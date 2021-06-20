b14624c8c8cef80c7fd8513ce7271cf3e1f73577
Fumiya Sakai <just1factory@gmail.com>
2021-06-21 03:42:23 +0900

    ディレクトリの移動

--- iOSApp/SimpleMoviePlayerExample/ViewController/MainViewController.swift
+++ iOSApp/SimpleMoviePlayerExample/ViewController/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  SimpleMoviePlayerExample
 //
 //  Created by 酒井文也 on 2021/05/03.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MainViewController: UIViewController {
+
+    // MARK: - Override
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
