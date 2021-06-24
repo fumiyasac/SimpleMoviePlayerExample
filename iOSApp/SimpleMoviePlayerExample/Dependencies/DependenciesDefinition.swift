9ec97514af720d945949446c63d9bc80001ae5c8
Fumiya Sakai <just1factory@gmail.com>
2021-06-24 23:37:49 +0900

    必要なライブラリ追加＆DI部分の追記

---
+++ iOSApp/SimpleMoviePlayerExample/Dependencies/DependenciesDefinition.swift
@@ -0,0 +1,50 @@
+//
+//  DependenciesDefinition.swift
+//  SimpleMoviePlayerExample
+//
+//  Created by 酒井文也 on 2021/06/24.
+//
+
+import Foundation
+import RxSwift
+
+final class DependenciesDefinition {
+
+    // MARK: - Function
+
+    func inject() {
+
+        // MEMO: 同じProtocolを適合させるがLocalに保持したデータを利用したい際につける名前
+        let local = "local"
+
+        // MEMO: 処理を実行する際にバックグラウンドスレッドにしたい際につける名前
+        let background = "background"
+
+        // MEMO: インスタンスを保持するための場所
+        let dependecies = DependeciesContainer.shared
+
+        // MARK: - Scheduler
+
+        // メインスレッド動作
+        dependecies.register(
+            ImmediateSchedulerType.self,
+            impl: MainScheduler.instance
+        )
+        // バックグラウンドスレッド動作
+        dependecies.register(
+            ImmediateSchedulerType.self,
+            impl: SerialDispatchQueueScheduler(qos: .default),
+            name: background
+        )
+
+        // MARK: - Infra
+
+        // MEMO: (1) UserDefault関連処理部分
+
+        // MARK: - Repository
+
+        // MARK: - Scheduler
+
+        // MARK: - Presenter
+    }
+}
