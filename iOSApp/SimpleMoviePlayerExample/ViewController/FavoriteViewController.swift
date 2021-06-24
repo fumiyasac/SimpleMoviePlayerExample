92e0dc41b9027f96f1915fb5a884b8ba15b87c47
Fumiya Sakai <just1factory@gmail.com>
2021-06-24 23:20:08 +0900

    GlobalTabBarに乗せる画面要素を作成する

---
+++ iOSApp/SimpleMoviePlayerExample/ViewController/FavoriteViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteViewController.swift
+//  SimpleMoviePlayerExample
+//
+//  Created by 酒井文也 on 2021/06/24.
+//
+
+import UIKit
+
+final class FavoriteViewController: UIViewController {
+
+    // MARK: - Override
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ iOSApp/SimpleMoviePlayerExample/ViewController/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  SimpleMoviePlayerExample
+//
+//  Created by 酒井文也 on 2021/06/24.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+
+    // MARK: - Override
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
