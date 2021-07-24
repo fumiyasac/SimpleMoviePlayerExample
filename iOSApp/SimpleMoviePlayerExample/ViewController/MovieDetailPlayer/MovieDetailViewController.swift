079217a441faed71bf6b8644002e270476cdfdfe
Fumiya Sakai <just1factory@gmail.com>
2021-07-24 16:08:54 +0900

    Group整理と新規画面作成

---
+++ iOSApp/SimpleMoviePlayerExample/ViewController/MovieDetailPlayer/MovieDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDetailViewController.swift
+//  SimpleMoviePlayerExample
+//
+//  Created by 酒井文也 on 2021/07/24.
+//
+
+import UIKit
+
+final class MovieDetailViewController: UIViewController {
+
+    // MARK: - Override
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
