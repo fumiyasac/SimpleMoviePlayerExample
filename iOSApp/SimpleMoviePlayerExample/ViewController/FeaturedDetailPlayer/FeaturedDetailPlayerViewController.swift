451c12e4d6663b375cb5b7b7a949870cf3ea0af4
Fumiya Sakai <just1factory@gmail.com>
2021-07-31 21:24:10 +0900

    特集表示用新規コントローラー

---
+++ iOSApp/SimpleMoviePlayerExample/ViewController/FeaturedDetailPlayer/FeaturedDetailPlayerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FeaturedDetailPlayerViewController.swift
+//  SimpleMoviePlayerExample
+//
+//  Created by 酒井文也 on 2021/07/31.
+//
+
+import UIKit
+
+final class FeaturedDetailPlayerViewController: UIViewController {
+
+    // MARK: - Override
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
