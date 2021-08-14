6471e20d4e6c7f4553b1e643679fee4d17c1766c
Fumiya Sakai <just1factory@gmail.com>
2021-08-14 18:31:27 +0900

    Main画面に表示するCarousel表示部分に関する内容を詰め込んだDataTransferObjectを定義

--- iOSApp/SimpleMoviePlayerExample/UseCase/DataTransferObject/CarouselMainBannerDto.swift
+++ iOSApp/SimpleMoviePlayerExample/UseCase/DataTransferObject/CarouselMainBannerDto.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+final class CarouselMainBannerDto: Equatable {
+
+    let mainBanners: [MainBannerEntity]
+
+    // MARK: - Initializer
+
+    init(
+        mainBanners: [MainBannerEntity]
+    ) {
+        self.mainBanners = mainBanners
+    }
+
+    // MARK: - Equatable
+
+    static func == (
+        lhs: CarouselMainBannerDto,
+        rhs: CarouselMainBannerDto
+    ) -> Bool {
+        return lhs.mainBanners == rhs.mainBanners
+    }
+}
