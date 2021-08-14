62de8153a99675eca297ebc75a5e94bfe2a7fe38
Fumiya Sakai <just1factory@gmail.com>
2021-08-14 18:31:55 +0900

    Main画面に表示するUseCaseを定義

--- iOSApp/SimpleMoviePlayerExample/UseCase/MainUseCase.swift
+++ iOSApp/SimpleMoviePlayerExample/UseCase/MainUseCase.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+import RxSwift
+
+//sourcery: AutoMockable
+protocol MainUseCase {
+    // 画面表示に必要なデータを返す
+    func execute() -> Single<MainDto>
+}
+
+final class MainUseCaseImpl: MainUseCase {
+
+    private let initialAppOpenRepository: InitialAppOpenRepository
+    private let mainNewsRepository: MainNewsRepository
+    private let featuredMovieRepository: FeaturedMovieRepository
+    private let mainMovieRepository: MainMovieRepository
+
+    // MARK: - Initializer
+
+    init(
+        initialAppOpenRepository: InitialAppOpenRepository,
+        mainNewsRepository: MainNewsRepository,
+        featuredMovieRepository: FeaturedMovieRepository,
+        mainMovieRepository: MainMovieRepository
+    ) {
+        self.initialAppOpenRepository = initialAppOpenRepository
+        self.mainNewsRepository = mainNewsRepository
+        self.featuredMovieRepository = featuredMovieRepository
+        self.mainMovieRepository = mainMovieRepository
+    }
+
+    // MARK: - MainUseCase
+
+    func execute() -> Single<MainDto> {
+
+        // MEMO: 任意の値取得処理を実行してflatMapを利用して返したい型へ変換する
+        return initialAppOpenRepository.get().flatMap { [weak self] shouldShowToolTip in
+            guard let weakSelf = self else {
+                return Single.error(MoviePlayerError.notExistSelf)
+            }
+            // MEMO: Single.zipを利用して他処理の実行を待ってから返したい型へ変換を試みる
+            return Single.zip(
+                weakSelf.mainNewsRepository.findAll(),
+                weakSelf.featuredMovieRepository.findAll(),
+                weakSelf.mainMovieRepository.findAll()
+            ).map { apiResponses -> MainDto in
+                // MEMO: APIレスポンス取得結果とUserDefaultの処理結果を組み合わせてMainDtoを作成する
+                let (mainNews, featuredMovies, mainMovies) = apiResponses
+                return MainDto(
+                    shouldShowToolTip: shouldShowToolTip,
+                    mainNews: mainNews,
+                    featuredMovies: featuredMovies,
+                    mainMovies: mainMovies
+                )
+            }
+        }
+    }
+}
