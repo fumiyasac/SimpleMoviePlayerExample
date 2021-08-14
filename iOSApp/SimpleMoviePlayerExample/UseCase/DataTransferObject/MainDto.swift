3a9c26ba075680139f0adecdcb7b2803ccdb3340
Fumiya Sakai <just1factory@gmail.com>
2021-08-14 18:31:08 +0900

    Main画面に表示する内容を詰め込んだDataTransferObjectを定義

--- iOSApp/SimpleMoviePlayerExample/UseCase/DataTransferObject/MainDto.swift
+++ iOSApp/SimpleMoviePlayerExample/UseCase/DataTransferObject/MainDto.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+final class MainDto: Equatable {
+
+    let shouldShowToolTip: Bool
+    let mainNews: [MainNewsEntity]
+    let featuredMovies: [FeaturedMovieEntity]
+    let mainMovies: [MainMovieEntity]
+
+    // MARK: - Initializer
+
+    init(
+        shouldShowToolTip: Bool,
+        mainNews: [MainNewsEntity],
+        featuredMovies: [FeaturedMovieEntity],
+        mainMovies: [MainMovieEntity]
+    ) {
+        self.shouldShowToolTip = shouldShowToolTip
+        self.mainNews = mainNews
+        self.featuredMovies = featuredMovies
+        self.mainMovies = mainMovies
+    }
+
+    // MARK: - Equatable
+
+    static func == (
+        lhs: MainDto,
+        rhs: MainDto
+    ) -> Bool {
+        return lhs.shouldShowToolTip == rhs.shouldShowToolTip &&
+            lhs.mainNews == rhs.mainNews &&
+            lhs.featuredMovies == rhs.featuredMovies &&
+            lhs.mainMovies == rhs.mainMovies
+    }
+}
