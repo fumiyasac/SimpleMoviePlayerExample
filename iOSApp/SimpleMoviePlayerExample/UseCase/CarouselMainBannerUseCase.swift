a4a2eb627a297320105d2903340d4658a26b285d
Fumiya Sakai <just1factory@gmail.com>
2021-08-14 18:32:19 +0900

    Main画面に表示するCarousel表示部分に関するUseCaseを定義

--- iOSApp/SimpleMoviePlayerExample/UseCase/CarouselMainBannerUseCase.swift
+++ iOSApp/SimpleMoviePlayerExample/UseCase/CarouselMainBannerUseCase.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import RxSwift
+
+//sourcery: AutoMockable
+protocol CarouselMainBannerUseCase {
+    // 画面表示に必要なデータを返す
+    func execute() -> Single<CarouselMainBannerDto>
+}
+
+final class CarouselMainBannerUseCaseImpl: CarouselMainBannerUseCase {
+
+    private let mainBannerRepository: MainBannerRepository
+
+    // MARK: - Initializer
+
+    init(
+        mainBannerRepository: MainBannerRepository
+    ) {
+        self.mainBannerRepository = mainBannerRepository
+    }
+
+    // MARK: - CarouselMainBannerUseCase
+
+    func execute() -> Single<CarouselMainBannerDto> {
+        return mainBannerRepository.findAll().flatMap { mainBanners in
+            return Single.just(CarouselMainBannerDto(mainBanners: mainBanners))
+        }
+    }
+}
