a3e5b0583cccabc2f323ebf96206585ef07c24ac
Fumiya Sakai <just1factory@gmail.com>
2021-10-06 00:06:47 +0900

    画面遷移を管理するためのCoodinatorの導入

---
+++ iOSApp/SimpleMoviePlayerExample/Coodinator/MainScreenCoodinator.swift
@@ -0,0 +1,11 @@
+//
+//  MainScreenCoodinator.swift
+//  SimpleMoviePlayerExample
+//
+//  Created by 酒井文也 on 2021/10/05.
+//
+
+import Foundation
+
+// sourcery: AutoMockable
+protocol ItemsScreenCoodinator: ScreenCoordinator {}
---
+++ iOSApp/SimpleMoviePlayerExample/Coodinator/ScreenCoordinator.swift
@@ -0,0 +1,10 @@
+//
+//  ScreenCoordinator.swift
+//  SimpleMoviePlayerExample
+//
+//  Created by 酒井文也 on 2021/10/05.
+//
+
+import Foundation
+
+protocol ScreenCoordinator: AnyObject {}
