28bc0216985a7e95d76e527a7978c12c38bf63b5
fumiyasac <just1factory@gmail.com>
2024-09-15 21:57:48 +0900

    新たにViewStateProviderを作成する

---
+++ iOSApp/SimpleMoviePlayerExample/Presentation/ViewStateProvider/SettingsViewStateProvider.swift
@@ -0,0 +1,12 @@
+//
+//  SettingsViewStateProvider.swift
+//  SimpleMoviePlayerExample
+//
+//  Created by 酒井文也 on 2024/09/15.
+//
+
+import Foundation
+import Observation
+
+@Observable
+final class SettingsViewStateProvider {}
